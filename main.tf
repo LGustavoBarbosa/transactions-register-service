ed5c47341f27463d229d6062d36ea4ece7633fe2
Luiz Gustavo <gflashwolf@gmail.com>
2025-02-19 16:52:58 -0300

    stage name deprecated

--- main.tf
+++ main.tf
@@ -129,7 +129,6 @@ resource "aws_api_gateway_integration" "get_lambda_integration" {
 
 resource "aws_api_gateway_deployment" "transaction_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.transaction_api.id
-  stage_name  = var.stage_name
 
   depends_on = [
     aws_api_gateway_integration.post_lambda_integration,
